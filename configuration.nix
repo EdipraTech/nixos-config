b369d0474c7af568ddabdf56558214d05d5d0dbb
Daniel Arpide <edipra.tech@gmail.com>
2024-06-25 12:28:07 -0400

    nix updates

--- configuration.nix
+++ configuration.nix
@@ -28,13 +28,20 @@
 # networking.proxy.default = "http://user:password@proxy:port/";
 # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-# Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
-# console = {
-#   font = "Lat2-Terminus16";
-#   keyMap = "us";
-#   useXkbConfig = true; # use xkbOptions in tty.
-# };
+# Select internationalization properties
+i18n.defaultLocaleSettings = "en_US.UTF-8";
+
+i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+};
 
 # Enable the X11 windowing system.
     services.xserver = {
